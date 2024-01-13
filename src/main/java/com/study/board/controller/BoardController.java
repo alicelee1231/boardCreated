672f9c554cac0ce9df4c1760d42c77e26a6aab4b
alicelee1231 <saemmilee1231@gmail.com>
2024-01-13 21:52:27 +0900

    가 게시판 만들기 완료

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
 
 @Controller
 public class BoardController {
@@ -34,8 +39,26 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model){
-        model.addAttribute("list", boardService.boardList());
+    public String boardList(Model model,
+                            @PageableDefault(page = 0, size =10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                            String searchKeyword){
+
+        Page<Board> list = null;
+            //검색키워드가 안들어왔을 때
+        if(searchKeyword == null){
+            list = boardService.boardList(pageable);
+        }else{
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
+        int nowPage = list.getPageable().getPageNumber()+1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage",nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         return "boardList";
     }
 
