67e4cbcdde7d1279e2eacdba54f79e48b50d70b3
alicelee1231 <saemmilee1231@gmail.com>
2024-01-18 11:10:28 +0900

    write수정(새글 올릴때 빈값이면 저장안되게 하는거)

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,11 +3,16 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import com.study.board.service.BoardService;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.weaver.patterns.TypePatternQuestions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -15,6 +20,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +32,7 @@ import java.util.HashMap;
 
 @Controller
 @Slf4j
-public class BoardController {
+public class BoardController  {
 
     private static final Logger logger = LoggerFactory.getLogger(BoardService.class);
 
@@ -47,21 +53,21 @@ public class BoardController {
     private BoardRepository boardRepository;
 
     @GetMapping("/board/write")
-    public String boardWriteForm(Board board, Model model){
+    public String boardWriteForm( Board board, Model model){
 
         return "boardwrite";
     }
 
-    @PostMapping("/board/writepro")
-    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
-
-        boardService.write(board,file);
-
-        model.addAttribute("message","글 작성이 완료되었습니다.");
-        model.addAttribute("searchUrl","/board/list");
-
-        return "message";
-    }
+//    @PostMapping("/board/writepro")
+//    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
+//
+//        boardService.write(board,file);
+//
+//        model.addAttribute("message","글 작성이 완료되었습니다.");
+//        model.addAttribute("searchUrl","/board/list");
+//
+//        return "message";
+//    }
 
     @GetMapping("/board/list")
     public String boardList(Model model,
@@ -116,7 +122,7 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate( @PathVariable("id") Integer id, Board board, Model model,  MultipartFile file) throws Exception{
+    public String boardUpdate( @PathVariable("id") Integer id, Board board, Model model, MultipartFile file) throws Exception{
 
         model.addAttribute("message", "글 수정이 완료되었습니다.");
         model.addAttribute("searchUrl", "/board/list");
@@ -124,8 +130,21 @@ public class BoardController {
         return "message";
     }
     @PostMapping("/board/save")
-    public String boardSave(  Board board, Model model,  MultipartFile file) throws Exception{
+    public String boardSave(HttpServletRequest request, Board board, Model model) throws Exception{
+        System.out.println(board.getTitle());
 
+        if(board.getTitle().isEmpty()){
+            System.out.println();
+            request.setAttribute("message","타이틀을 입력해 주세요");
+            model.addAttribute("searchUrl", "/board/write");
+
+            return "message";
+        }else if(board.getContent().isEmpty()) {
+            request.setAttribute("message","컨텐츠를 입력해주세요");
+            model.addAttribute("searchUrl", "/board/write");
+
+            return "message";
+        }
         model.addAttribute("message", "글 등록이 완료되었습니다.");
         model.addAttribute("searchUrl", "/board/list");
         boardRepository.save(board);
--- src/main/java/com/study/board/entity/Board.java
+++ src/main/java/com/study/board/entity/Board.java
@@ -2,6 +2,8 @@ package com.study.board.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -12,7 +14,13 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotNull
+    @NotEmpty
     private String title;
+
+    @NotNull
+    @NotEmpty
     private String content;
 
     private String filename;
